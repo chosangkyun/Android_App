d59e9b38dffc414bbe22570e6bb863f8adadabe0
chosangkyun <skcho0101@nate.com>
2016-07-28 14:56:50 +0900

    수정1

--- Prada_App1/MainActivity.cs
+++ Prada_App1/MainActivity.cs
@@ -19,12 +19,90 @@ namespace Prada_App1
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            ImageButton button1 = FindViewById<ImageButton>(Resource.Id.imageButton1);
+            button1.Click += OnimageButton1Clicked;
+            ImageButton button2 = FindViewById<ImageButton>(Resource.Id.imageButton2);
+            button2.Click += OnimageButton2Clicked;
+            ImageButton button3 = FindViewById<ImageButton>(Resource.Id.imageButton3);
+            button3.Click += OnimageButton3Clicked;
+            ImageButton button4 = FindViewById<ImageButton>(Resource.Id.imageButton4);
+            button4.Click += OnimageButton4Clicked;
+            ImageButton button5 = FindViewById<ImageButton>(Resource.Id.imageButton5);
+            button5.Click += OnimageButton5Clicked;
+            ImageButton button6 = FindViewById<ImageButton>(Resource.Id.imageButton6);
+            button6.Click += OnimageButton6Clicked;
+            ImageButton button7 = FindViewById<ImageButton>(Resource.Id.imageButton7);
+            button7.Click += OnimageButton7Clicked;
+            ImageButton button8 = FindViewById<ImageButton>(Resource.Id.imageButton8);
+            button8.Click += OnimageButton8Clicked;
+            ImageButton button9 = FindViewById<ImageButton>(Resource.Id.imageButton9);
+            button9.Click += OnimageButton9Clicked;
+            ImageButton button10 = FindViewById<ImageButton>(Resource.Id.imageButton10);
+            button10.Click += OnimageButton10Clicked;
+            ImageButton button11 = FindViewById<ImageButton>(Resource.Id.imageButton11);
+            button11.Click += OnimageButton11Clicked;
+            ImageButton button12 = FindViewById<ImageButton>(Resource.Id.imageButton12);
+            button12.Click += OnimageButton12Clicked;
+            ImageButton button13 = FindViewById<ImageButton>(Resource.Id.imageButton13);
+            button13.Click += OnimageButton13Clicked;
+        }
+        void OnimageButton1Clicked(object sender, EventArgs e)
+        {
+            setMessage("prada");
+        }
+        void OnimageButton2Clicked(object sender, EventArgs e)
+        {
+            setMessage("prada");
+        }
+        void OnimageButton3Clicked(object sender, EventArgs e)
+        {
+            setMessage("prada");
+        }
+        void OnimageButton4Clicked(object sender, EventArgs e)
+        {
+            setMessage("prada");
+        }
+        void OnimageButton5Clicked(object sender, EventArgs e)
+        {
+            setMessage("prada");
+        }
+        void OnimageButton6Clicked(object sender, EventArgs e)
+        {
+            setMessage("prada");
+        }
+        void OnimageButton7Clicked(object sender, EventArgs e)
+        {
+            setMessage("Power");
+        }
+        void OnimageButton8Clicked(object sender, EventArgs e)
+        {
+            setMessage("Setting");
+        }
+        void OnimageButton9Clicked(object sender, EventArgs e)
+        {
+            setMessage("Multi_Pad");
+        }
+        void OnimageButton10Clicked(object sender, EventArgs e)
+        {
+            setMessage("R/L_Pad");
+        }
+        void OnimageButton11Clicked(object sender, EventArgs e)
+        {
+            setMessage("Foot_Pad");
+        }
+        void OnimageButton12Clicked(object sender, EventArgs e)
+        {
+            setMessage("Rect_Pad");
+        }
+        void OnimageButton13Clicked(object sender, EventArgs e)
+        {
+            setMessage("Abdomen_Pad");
+        }
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        public void setMessage(string strMsg)
+        {
+            Toast message = Toast.MakeText(this, strMsg, ToastLength.Short);
+            message.Show();
         }
     }
 }
--- Prada_App1/Resources/Resource.Designer.cs
+++ Prada_App1/Resources/Resource.Designer.cs
@@ -60,8 +60,83 @@ namespace Prada_App1
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int imageButton1 = 2131034113;
+			
+			// aapt resource value: 0x7f050013
+			public const int imageButton10 = 2131034131;
+			
+			// aapt resource value: 0x7f050014
+			public const int imageButton11 = 2131034132;
+			
+			// aapt resource value: 0x7f050015
+			public const int imageButton12 = 2131034133;
+			
+			// aapt resource value: 0x7f050016
+			public const int imageButton13 = 2131034134;
+			
+			// aapt resource value: 0x7f050002
+			public const int imageButton2 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int imageButton3 = 2131034115;
+			
+			// aapt resource value: 0x7f050017
+			public const int imageButton4 = 2131034135;
+			
+			// aapt resource value: 0x7f050018
+			public const int imageButton5 = 2131034136;
+			
+			// aapt resource value: 0x7f050019
+			public const int imageButton6 = 2131034137;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int imageButton7 = 2131034112;
+			
+			// aapt resource value: 0x7f050011
+			public const int imageButton8 = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int imageButton9 = 2131034130;
+			
+			// aapt resource value: 0x7f050004
+			public const int imageView1 = 2131034116;
+			
+			// aapt resource value: 0x7f05000d
+			public const int imageView10 = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int imageView11 = 2131034126;
+			
+			// aapt resource value: 0x7f05000f
+			public const int imageView12 = 2131034127;
+			
+			// aapt resource value: 0x7f050010
+			public const int imageView13 = 2131034128;
+			
+			// aapt resource value: 0x7f050005
+			public const int imageView2 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int imageView3 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int imageView4 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int imageView5 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int imageView6 = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int imageView7 = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int imageView8 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int imageView9 = 2131034124;
 			
 			static Id()
 			{
