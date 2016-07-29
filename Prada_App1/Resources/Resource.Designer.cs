6b5620201653585ca26a9d9260b53de15edb4f72
chosangkyun <skcho0101@nate.com>
2016-07-29 11:05:48 +0900

    이미지 이름 공백 삭제

--- Prada_App1/Resources/Resource.Designer.cs
+++ Prada_App1/Resources/Resource.Designer.cs
@@ -45,10 +45,43 @@ namespace Prada_App1
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int abdomen_pad = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int mode_icon1 = 2130837505;
+			public const int foot_pad = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int heat_icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int Icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int left_Right_pad = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int low_icon = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int mode_icon = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int multipad = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int option_pad = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int sound_icon = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int square_pad = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int time_icon = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int ultra_icon = 2130837516;
 			
 			static Drawable()
 			{
